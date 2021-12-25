daa595e6c4f3025f9e389fa10a87e5faf02045d9
Harshanand Nyshadham <harsha.nyshadham@gmail.com>
2021-12-24 23:33:29 -0600

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -1,11 +1,23 @@
 import pandas as pd
 
+def generate_sch_from_list(lst):
+  temp=[]
+  for i in range(len(lst)):
+    for j in range(i+1,len(lst)):
+      temp.append([lst[i],lst[j],'x'])
+
+  return temp
+
 def create_new_season(filename):
  df=pd.read_excel(filename, engine ='openpyxl',sheet_name ='Groups')
- schedule=pd.DataFrame()
+ grpdata=pd.DataFrame()
  num_of_groups= len(df.columns)
  for i in range(num_of_groups):
-    schedule[df.columns[i]]=df.iloc[:,i]
+    grpdata[df.columns[i]]=df.iloc[:,i]
 
- print(schedule)
- return schedule
+ schedule_data=generate_sch_from_list(df.iloc[:,0])
+ print(schedule_data)
+ return grpdata
+
+
+
