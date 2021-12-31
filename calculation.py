247f4e82530a9fad5e95066e168b093c1eed3853
Harshanand Nyshadham <harsha.nyshadham@gmail.com>
2021-12-30 19:52:52 -0600

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -13,8 +13,11 @@ def create_new_season(filename):
  grpdata=pd.DataFrame()
  num_of_groups= len(df.columns)
  schedule_data=[]
+ player_list=[]
+
  for i in range(num_of_groups):
     grpdata[df.columns[i]]=df.iloc[:,i]
+    player_list.extend(df.iloc[:,i])
  for i in range(num_of_groups):
     schedule_data.extend(generate_sch_from_list(df.iloc[:,i]))
 
@@ -22,6 +25,10 @@ def create_new_season(filename):
  #writer.to_excel(filename,sheet_name='Schedule')
  with pd.ExcelWriter(filename,engine='openpyxl',mode='a') as wr:
                      writer.to_excel(wr,sheet_name='Schedule')
+
+
+ print(player_list)
+
  print(writer)
  return num_of_groups
 
