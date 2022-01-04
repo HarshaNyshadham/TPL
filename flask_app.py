645b088563587dbc79b763eb0a5a38f34dd86b80
Harshanand Nyshadham <harsha.nyshadham@gmail.com>
2022-01-03 19:44:57 -0600

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -112,7 +112,7 @@ def submitScore():
       #update score
       if(update_score(TPL_currentSeason,TPL_leaderboard,row_index,player1,player2,p1s1,p1s2,p1s3,p2s1,p2s2,p2s3,p1forefeit,p2forefeit)):
         return redirect(url_for('schedule'))
-      else
+      else:
         error="incorrect score"
         return render_template('enterScore.html',p1=player1,p2=player2,row_index=row_index,error=error)
   return redirect(url_for('enterScore'))
