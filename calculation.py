3becf549c0c042c60744095b8059e496d93fb86a
Harshanand Nyshadham <harsha.nyshadham@gmail.com>
2022-04-19 16:44:23 -0500

    a

--- calculation.py
+++ calculation.py
@@ -138,7 +138,7 @@ def update_points_doubles(filename,p1,p1points,p2,p2points,winner,bonusplayer,ga
   df.at[p1index,'GamesWon']=df.at[p1index,'GamesWon']+gamest1
   df.at[p1index,'GamesTotal']=df.at[p1index,'GamesTotal']+gamest1
   df.at[p2index,'GamesWon']=df.at[p2index,'GamesWon']+gamest2
-  df.at[p2index,'GamesTotal']=df.at[p1index,'GamesTotal']+gamest2
+  df.at[p2index,'GamesTotal']=df.at[p2index,'GamesTotal']+gamest2
 
   df.at[p1index,'%games']=(df.at[p1index,'GamesWon']/df.at[p1index,'GamesTotal'])*100
   df.at[p2index,'%games']=(df.at[p2index,'GamesWon']/df.at[p2index,'GamesTotal'])*100
