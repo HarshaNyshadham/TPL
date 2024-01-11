f197248897378e9e7b3c9e4ee79cbd5dc972a8fb
Harshanand Nyshadham <nash.nash@pranaam.tech>
2024-01-10 19:42:39 -0600

    asdas

--- flask_app.py
+++ flask_app.py
@@ -444,7 +444,33 @@ def doublesubmitscore45():
 
 @app.route('/DoublesLeaderboard',methods=['GET', 'POST'])
 def DoublesLeaderboard():
-    return render_template('DoubleLB.html')
+     
+
+    df_PT=pd.read_excel('/home/tpl/mysite/uploads/TPL_Doubles.xlsx', engine ='openpyxl',sheet_name ='PointTable',keep_default_na=False)
+    df_PT.sort_values(by=['Points','%games'],inplace =True,ascending=[False,False])
+    df_PT['%games']=round(df_PT['%games'].astype(float),2)
+    # df['Player']=df['Player'].astype(str)
+    # df_PT['Player']=df_PT['Player'].astype(str)
+
+    # df_new=df.merge(df_PT[['Player','Points','%games']],on = 'Player', how = 'left')
+    # df_new=df_new.fillna(0)
+    # print(df_new.columns)
+    # df_new['Points']=df_new['Points'].astype(int)
+    # df_new.sort_values(by=['Active','Points','%games'],inplace =True,ascending=[True,False,False])
+    
+    
+    
+    data_50=[]
+
+    for index,row in df_PT.iterrows():
+                  data_50.append([row['Team'],row['Points'],row['%games'],row['Matches']])
+                  # if(float(row['Division'])==4.5):
+                  #   data_45.append([row['Player'],row['Points'],row['%games'],row['Matches']])
+                  # elif(float(row['Division'])==4.0):
+                  #   data_40.append([row['Player'],row['Points'],row['%games'],row['Matches']])
+
+    return render_template('DoubleLB.html',data_50=data_50)
+    
 
 
 
