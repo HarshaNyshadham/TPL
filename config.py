8fc5683b6fc961b54ea7be40d48bda0bb96759a3
Harshanand Nyshadham <nash.nash@pranaam.tech>
2024-02-21 19:53:53 -0600

    asdas

---
+++ config.py
@@ -0,0 +1,5 @@
+#some file names
+
+TPL_leaderboard='/home/tpl/mysite/uploads/TPL_Leaderboard.xlsx'
+TPL_currentSeason='/home/tpl/mysite/uploads/TPL_currentseason.xlsx'
+Playoff_filename='/home/tpl/mysite/uploads/Playoff.xlsx'
