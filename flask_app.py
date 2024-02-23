0bf7eb225c352ff392f490a3ede01a6f88cf70b6
Harshanand Nyshadham <nash.nash@pranaam.tech>
2024-02-22 20:45:25 -0600

    asdsa

--- flask_app.py
+++ flask_app.py
@@ -72,7 +72,7 @@ def newindex():
 @app.route('/test')
 def test():
 
-    df_PT=pd.read_excel(TPL_Doubles_test, engine ='openpyxl',sheet_name ='PointTable',keep_default_na=False)
+    df_PT=pd.read_excel(TPL_currentSeason, engine ='openpyxl',sheet_name ='PointTable',keep_default_na=False)
     df_PT.sort_values(by=['Points','%games'],inplace =True,ascending=[False,False])
     df_PT['%games']=round(df_PT['%games'].astype(float),2)
     
