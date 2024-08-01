78ad2579a15061c8b3fa7ad9a43b4f9327fd3303
Harshanand Nyshadham <harshanand.n@ishausa.org>
2024-08-01 10:18:40 -0500

    asd

--- config.py
+++ config.py
@@ -35,3 +35,30 @@ def get_LeaderBoard(league='Singles'):
                     data_40.append([row[name_col],row['Points'],row['%games'],row['Matches']])
 
     return data_50,data_45,data_40
+
+#get leaderboard sorted by points then %games
+#returns leaderboard data dict by Div 
+def get_LeaderBoard_League(league='Singles'):
+    name_col=''
+    if league=='Singles':
+             df_PT=pd.read_excel(TPL_currentSeason, engine ='openpyxl',sheet_name ='PointTable',keep_default_na=False)
+             name_col='Player'
+    elif league=='Doubles':
+            df_PT=pd.read_excel(TPL_Doubles, engine ='openpyxl',sheet_name ='PointTable',keep_default_na=False)
+            name_col='Team'
+    df_PT.sort_values(by=['Points','%games'],inplace =True,ascending=[False,False])
+    df_PT['%games']=round(df_PT['%games'].astype(float),2)
+    
+    data_45=[]
+    data_40=[]
+    data_50=[]
+
+    for index,row in df_PT.iterrows():
+                  if(float(row['Division'])==5.0):
+                    data_50.append([row[name_col],row['Points'],row['%games'],row['Matches']])
+                  if(float(row['Division'])==4.5):
+                    data_45.append([row[name_col],row['Points'],row['%games'],row['Matches']])
+                  elif(float(row['Division'])==4.0):
+                    data_40.append([row[name_col],row['Points'],row['%games'],row['Matches']])
+
+    return data_50,data_45,data_40
